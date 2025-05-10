f98034621674a6cf796449c7ee06d3afb1636ee4
Aureo Dantas <dantasaureo19@gmail.com>
2025-05-10 05:50:39 -0300

    Initial commit

---
+++ src/main/java/com/insurance/budget_connector/BudgetConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.budget_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insurance/budget_connector/BudgetConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.budget_connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
